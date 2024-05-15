fdf6f93a781337e1fc87c80e99454ab1dcbdfa3e
Hoàng Nguyễn Việt <hoangnguyenviet@Hoangs-MacBook-Pro.local>
2024-05-15 14:40:58 +0700

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <main>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <keep-alive include="CreatePost">
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
   </main>
 </template>
 
--- src/components/pages/BlogDetail.vue
+++ src/components/pages/BlogDetail.vue
@@ -1,22 +1,40 @@
 <template>
   <main-content>
     <v-container>
-      <h2 class="mb-3">{{ blogItem.blogTitle }}</h2>
-      <h4 class="mb-3">Posted on: {{ blogItem.createdAt }}</h4>
-      <div class="img mb-3 d-flex justify-center">
-        <img :src="blogItem.blogImg" alt="" />
+      <h2 class="py-10 title">{{ blogItem.blogTitle }}</h2>
+      <div class="d-flex py-10">
+        <div class="v-col-3 pr-5">
+          <h4>Author: {{ composer }}</h4>
+          <h4 class="pb-8 btom">{{ username }}</h4>
+          <h4 class="py-8 btom">Posted on: {{ blogItem.createdAt }}</h4>
+          <h4 class="pt-8">
+            <router-link class="text-green-darken-3" :to="{ name: 'Blogs' }">
+              <v-icon>mdi-arrow-left</v-icon>
+              Back to the blog
+            </router-link>
+          </h4>
+        </div>
+        <div class="pl-5 content">
+          <div class="img">
+            <img :src="blogItem.blogImg" alt="" />
+          </div>
+          <div class="post-content ql-editor" v-html="blogItem.blogHTML"></div>
+        </div>
       </div>
-      <div
-        class="post-content ql-editor"
-        v-html="blogItem.blogHTML"
-      ></div>
     </v-container>
   </main-content>
 </template>
 
 <script>
 import { auth, blogs } from "@/state/helpers";
+// import { getAuth } from "firebase/auth";
 export default {
+  data() {
+    return {
+      composer: null,
+      username: null,
+    };
+  },
   computed: {
     ...blogs.blogsComputed,
     ...auth.authComputed,
@@ -25,13 +43,21 @@ export default {
     ...blogs.blogsMethods,
     ...auth.authMethods,
     async intital() {
-      await this.getCurrentBlog(this.$route.params.blogid)
+      await this.getCurrentBlog(this.$route.params.blogid);
+    },
+    async getAuthor() {
+      await this.getAllUsers();
+      const author = this.listUsers.filter((user) => {
+        return user.id === this.blogItem.author;
+      });
+      this.composer = `${author[0].firstName} ${author[0].lastName}`;
+      this.username = author[0].username;
     },
   },
   async created() {
-    await this.getAllBlogs()
-    await this.intital()
-    await this.getAllUsers()
+    await this.getAllBlogs();
+    await this.intital();
+    await this.getAuthor();
   },
 };
 </script>
@@ -47,9 +73,25 @@ h2 {
   font-weight: 700;
 }
 h4 {
-  text-align: center;
+  text-align: end;
+}
+a {
+  text-decoration: none;
 }
 img {
   width: 800px;
+  padding-left: 15px;
+}
+.title {
+  border-bottom: 1px solid #ccc;
+}
+.content {
+  border-left: 1px solid #ccc;
+}
+.btom {
+  border-bottom: 1px solid #ccc;
+}
+.ql-editor {
+  height: auto;
 }
 </style>
